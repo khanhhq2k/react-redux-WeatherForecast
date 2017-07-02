6de5a4915f52724d31b1fe74ecca9c72f3dc7721
khanhhq2k <khanhhq2k@gmail.com>
2017-07-02 16:57:39 +0700

    init commit, create basic search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+import SearhBar from '../containers/search_bar';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearhBar />
+      </div>      
     );
   }
 }
